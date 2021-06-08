0185b04ed4f8385798e428c760fc0c7b27d78e89
delta7-138 <krishna.sharma@iiitb.org>
2021-06-08 17:29:42 +0530

    basic interface without backend

--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart'; 
 import 'package:flutter/cupertino.dart'; 
-import 'package:firebase_core/firebase_core.dart';
 
 class CreatePost extends StatefulWidget{
   @override 
@@ -9,7 +8,6 @@ class CreatePost extends StatefulWidget{
 
 class _CreatePostState extends State<CreatePost>{
   final _formKey = GlobalKey<FormState>();
-  final Future<FirebaseApp> _initObj = Firebase.initializeApp();
   
 
   @override 
@@ -20,62 +18,49 @@ class _CreatePostState extends State<CreatePost>{
     // if(! _init){
     //   return Center(child: Text('Loading right now'));
     // }
-    return FutureBuilder(
-      future: _initObj, 
-      builder: (context , snapshot){
-        if(snapshot.hasError){
-          return Center(child: Text('Caught exception' , style: TextStyle(color: Colors.white)));
+      return Scaffold(
+      body: ExpansionTile(title: Text('Create a thread' , style: TextStyle(color: Colors.white)) , children: [Form(
+      key: _formKey, 
+      child: Column(children: [
+        TextFormField(
+          validator: (value){
+            if(value==null || value.isEmpty){
+              return "Please enter a title";
+            }
+          },
+          style: TextStyle(
+            color: Colors.white
+          ), 
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(15),
+            hintText: 'Enter the title for the post', 
+            hintStyle: TextStyle(color: Colors.grey)
+          ),
+        ), 
+        TextFormField(
+          validator: (value){
+            if(value==null || value.isEmpty){
+              return "Please enter a description"; 
+            }
+          }, style:(
+            TextStyle(color: Colors.white)
+          ), 
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(15),
+            hintText: 'Enter the description for the post', 
+            hintStyle: TextStyle(color: Colors.grey)
+          )
+        ), 
+        ElevatedButton(child: Text('Submit'), 
+        onPressed: (){
+          if (_formKey.currentState!.validate()){
+            print('done');
+          }
         }
-
-        if(snapshot.connectionState== ConnectionState.done){
-              return Scaffold(
-              body: ExpansionTile(title: Text('Create a thread' , style: TextStyle(color: Colors.white)) , children: [Form(
-              key: _formKey, 
-              child: Column(children: [
-                TextFormField(
-                  validator: (value){
-                    if(value==null || value.isEmpty){
-                      return "Please enter a title";
-                    }
-                  },
-                  style: TextStyle(
-                    color: Colors.white
-                  ), 
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(15),
-                    hintText: 'Enter the title for the post', 
-                    hintStyle: TextStyle(color: Colors.grey)
-                  ),
-                ), 
-                TextFormField(
-                  validator: (value){
-                    if(value==null || value.isEmpty){
-                      return "Please enter a description"; 
-                    }
-                  }, style:(
-                    TextStyle(color: Colors.white)
-                  ), 
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(15),
-                    hintText: 'Enter the description for the post', 
-                    hintStyle: TextStyle(color: Colors.grey)
-                  )
-                ), 
-                ElevatedButton(child: Text('Submit'), 
-                onPressed: (){
-                  if (_formKey.currentState!.validate()){
-                    print('done');
-                  }
-                }
-                ,
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.deepPurple
-                ))
-              ],)
-            )])); 
-        }
-        return Center(child: Text('Loading!', style: TextStyle(color: Colors.white))); 
-      }
-    ); 
-  }
-}
+        ,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.deepPurple
+        ))
+      ],)
+    )])); 
+}}
